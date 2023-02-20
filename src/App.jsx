a737113f74b1c0b12ecbbfa353553dc8588383f6
Huy Dang <ghuydang@gmail.com>
2023-02-20 11:12:44 -0700

    Implement logic for picking cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import shuffle from './utilities/shuffle';
 
 function App() {
   const [cards, setCards] = useState(shuffle); // Cards array from assets
+  const [pickOne, setPickOne] = useState(null); // First selection
+  const [pickTwo, setPickTwo] = useState(null); // Second selection
+  const [disabled, setDisabled] = useState(false); // Delay handler
+
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card);
+    }
+  };
 
   return (
     <>
@@ -12,7 +21,12 @@ function App() {
           const { image, id, matched } = card;
 
           return (
-            <Card key={id} image={image} selected={false} onClick={() => {}} />
+            <Card
+              key={id}
+              image={image}
+              selected={card === pickOne || card === pickTwo || matched}
+              onClick={() => handleClick(card)}
+            />
           );
         })}
       </div>
